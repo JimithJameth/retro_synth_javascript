de1e5fdd7b3095062153decd2194692818c59e78
Jamie Martin <jamiemartinmusic@gmail.com>
2017-09-04 21:19:31 +0100

    managed to get reverb and lowpass filter working on osc1

--- public/bundle.js
+++ public/bundle.js
@@ -78,42 +78,106 @@ window.onload = () => {
     options: {
       type: 'sawtooth',
       volume: 0.1,
-          frequency: 261.63 // note C
+          frequency: 261.63,
+          attack: 0.2,
+          release: 0.8 // note C
         }
       });
+  var reverb = new Pizzicato.Effects.Reverb({
+      time: 1,
+      decay: 0.8,
+      reverse: true,
+      mix: 0.5
+  });
+  osc1.addEffect(reverb);
+
+  var lowPassFilter = new Pizzicato.Effects.LowPassFilter({
+      frequency: 500,
+      peak: 10
+  });
+
+  osc1.addEffect(lowPassFilter);
+  
 
   var osc2 = new Pizzicato.Sound({
     source: 'wave',
     options: {
-      type: 'square',
+      type: 'sawtooth',
       volume:0.1,
       frequency: 329.63 // note E
     }
   });
 
+  var reverb = new Pizzicato.Effects.Reverb({
+      time: 1,
+      decay: 0.8,
+      reverse: true,
+      mix: 0.5
+  });
+  osc2.addEffect(reverb);
+
   var osc3 = new Pizzicato.Sound({
     source: 'wave',
     options: {
-     type: 'sine',
+     type: 'sawtooth',
      volume:0.1,
      frequency: 392.00 // note G
    }
  });
 
-  var hash = {
-    a: 261.63, // c
-    w: 277.18, // c#
-    s: 293.66, // d
-    e: 311.13, // d#
-    d: 329.63, // e
-    f: 349.23, // f
-    t: 369.99, // f#
-    g: 392.00, // g
-    y: 415.30, // g#
-    h: 440.00, // a
-    u: 466.16, // a#
-    j: 493.88, // b 
-    k: 523.25, // c
+  // var lowPassFilter = new Pizzicato.Effects.LowPassFilter({
+  //     frequency: 400,
+  //     peak: 10
+  // });
+
+  // sound.addEffect(lowPassFilter);
+
+  var osc1Hash = {
+    a: 130.81, // c
+    w: 138.59, // c#
+    s: 146.83, // d
+    e: 155.56, // d#
+    d: 164.81, // e
+    f: 174.61, // f
+    t: 185.00, // f#
+    g: 196.00, // g
+    y: 207.65, // g#
+    h: 220.00, // a
+    u: 233.08, // a#
+    j: 246.94, // b 
+    k: 261.63, // c
+  }
+
+  var osc2Hash = {
+    a: 329.63, // e
+    w: 349.23, // f
+    s: 369.99, // f#
+    e: 392.00, // g
+    d: 415.30, // g#
+    f: 440.00, // a
+    t: 466.16, // a#
+    g: 493.88, // b 
+    y: 523.25, // c
+    h: 554.37, // c#
+    u: 587.33, // d
+    j: 622.25, // d#
+    k: 659.25, // e
+  }
+
+  var osc3Hash = {
+    a: 392.00, // g
+    w: 415.30, // g#
+    s: 440.00, // a
+    e: 466.16, // a#
+    d: 493.88, // b 
+    f: 523.25, // c
+    t: 554.37, // c#
+    g: 587.33, // d
+    y: 622.25, // d#
+    h: 659.25, // e
+    u: 698.46, // f
+    j: 739.99, // f#
+    k: 783.99, // g
   }
 
   const pitchSlider = document.getElementById('pitchOsc1');
@@ -150,12 +214,20 @@ window.onload = () => {
   let currentSound = null;
 
   body.addEventListener('keydown', function(event) {
+    // osc1.stop();
+    // osc2.stop();
+    // osc3.stop();
 
-    if(hash[event.key] !== null && hash[event.key] !== undefined) {
-      osc1.sourceNode.frequency.value = hash[event.key];
+    if(osc1Hash[event.key] !== null && osc1Hash[event.key] !== undefined) {
+      osc1.sourceNode.frequency.value = osc1Hash[event.key];
+      // osc2.sourceNode.frequency.value = osc2Hash[event.key];
+      // osc3.sourceNode.frequency.value = osc3Hash[event.key];
+
       osc1.play();
       osc2.play();
       osc3.play();
+
+      console.log(osc2Hash)
     }
 
     // if(event.key === 'c') {
